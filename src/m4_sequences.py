0fc31f3d6947a68af4e9ac626eb14fa06c361f77
stevenka <stevenka@rose-hulman.edu>
2019-03-28 22:40:14 -0400

    m3

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Keely Stevenson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # Students:
@@ -135,13 +135,19 @@ def practice_problem4a(sequence):
       :type sequence: list | tuple | string
     """
     ###########################################################################
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
+
+    new_list = []
+    for k in range(len(sequence) - 1):
+        if sequence[k] == sequence[k + 1]:
+            new_list = new_list + [k]
+    return new_list
 
 
 def run_test_practice_problem4b():
@@ -198,13 +204,19 @@ def practice_problem4b(sequence):
       :type sequence: (list | tuple) of (float | int)
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   10 minutes.
     ###########################################################################
+
+    largest = sequence[0]
+    for k in range(2, len(sequence), 2):
+        if sequence[k] > largest:
+            largest = sequence[k]
+    return largest
 
 
 def run_test_practice_problem4c():
@@ -296,7 +308,7 @@ def practice_problem4c(points):
       :rtype: rg.Point | string
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # IMPORTANT: This problem is your LOWEST PRIORITY for preparing
@@ -307,6 +319,18 @@ def practice_problem4c(points):
     #    DIFFICULTY:      9
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
+
+    for k in points:
+        if is_prime(k.x):
+            if is_prime(k.y):
+                xval = k.x
+                yval = k.y
+                k.x = yval
+                k.y = xval
+                return k
+    return 'Not found'
+
+
 
 
 def run_test_practice_problem4d():
@@ -392,13 +416,20 @@ def practice_problem4d(sequence):
       :rtype: int
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
+
+    sum = 0
+    for k in range(len(sequence) - 1):
+        if is_prime(sequence[k]):
+            if sequence[k] != sequence[k + 1] and is_prime(sequence[k + 1]):
+                sum = sum + sequence[k]
+    return sum
 
 
 # -----------------------------------------------------------------------------
